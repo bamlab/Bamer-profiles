82377d66d3680ea84f27d313d797fe7201363faf
MattAgn <matthieua@bam.tech>
2021-04-28 22:02:35 +0200

    fetch github api

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 import { RouteProp } from '@react-navigation/core';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { BamerProfile } from '../../types';
 
@@ -14,13 +15,26 @@ type Props = {
 export const Profile = ({ route: { params } }: Props) => {
   const { name, email, phoneNumber, githubHandle } = params;
 
+  const [repos, setRepos] = useState<
+    { link: string; nbStars: number; name: string }[]
+  >();
+
+  useEffect(() => {
+    // fetch the github repositories of the candidate
+    axios
+      .get('https://github/api/' + githubHandle + '/repos/')
+      .then(res => setRepos(res.data.repos));
+  }, []);
+
   return (
     <View>
       <Text>Detailed Profile</Text>
       <Text>Name: {name}</Text>
       <Text>Email: {email}</Text>
       <Text>Phone Number: {phoneNumber}</Text>
+
       <Text>Github handle: {githubHandle}</Text>
+      <Text>Number of repositories: {repos?.length}</Text>
     </View>
   );
 };
